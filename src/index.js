c45a8b6d19a42c9dbf6c3743831f042b9017dc1b
Marina korniychuk <kornij4uk@gmail.com>
2017-02-27 00:04:02 +0300

    flatten/feature: implement task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function flatten(array) {
-  // your solution
+    if (!Array.isArray(array))
+        return [];
+
+    function isNested(arr) {
+        return !arr.every(a => !Array.isArray(a))
+    }
+
+    while (isNested(array))
+        array = [].concat.apply([], array);
+    return array;
 }
